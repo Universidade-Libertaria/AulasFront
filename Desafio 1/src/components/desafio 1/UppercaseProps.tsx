00d10675faa90d7dfd0c9d0fdc3dc9a59b320554
MatheusVBLima <matheus.lima@ccc.ufcg.edu.br>
2023-01-19 19:41:00 -0300

    Add: imagem comentário

--- Desafio 1/src/components/desafio 1/UppercaseProps.tsx
+++ Desafio 1/src/components/desafio 1/UppercaseProps.tsx
@@ -1,9 +1,11 @@
 import styles from "./uppercaseProps.module.scss";
 
+//? Para fazer em JS, lembre que não precisa usar a interface ou type,
+//? basta usar props e, nesse caso, props.texto na hora de selecionar
 interface UppercaseProps {
   texto: string;
 }
 export function UppercaseProps({ texto }: UppercaseProps) {
-  const textoUpperCase = texto.toUpperCase();
-  return <h2 className={styles.wrapper}>{textoUpperCase}</h2>;
+  /* const textoUpperCase = texto.toUpperCase(); */
+  return <h2 className={styles.wrapper}>{texto.toUpperCase()}</h2>;
 }
